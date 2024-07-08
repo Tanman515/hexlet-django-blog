efb89c3f58a57b554cfcf72e9a86433296afe656
Oleg <antoshinov@gmail.com>
2024-07-08 19:03:25 +1000

    working with templates

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('article')
+    return render(request, 'articles/index.html', context={'app_name':__name__})
