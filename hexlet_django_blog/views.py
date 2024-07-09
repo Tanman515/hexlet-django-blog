33a8e58c1daa12986af1ec86781d415ecd24cc43
Oleg <antoshinov@gmail.com>
2024-07-09 20:01:04 +1000

    adding template_view

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-def index(request):
-    return render(request, 'index.html')
+class HomePageView(TemplateView):
+
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'World'
+        return context
 
 
 def about(request):
