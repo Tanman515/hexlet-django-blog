5a7c7133b6f6840a7133f93da758939af65378bb
Oleg <antoshinov@gmail.com>
2024-07-14 20:11:53 +1000

    add model

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse
 
 class MyView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'articles/index.html', context={'app_name':__name__})
 
+    def get(self, request, *args, **kwargs):
+        return redirect(reverse('article', kwargs={'tags': 'python', 'article_id': 42}))
+
+
+def index(request, tags, article_id):
+    return HttpResponse(f'Статья номер {article_id}. Тег {tags}')
