df9849ef2542d50c919388fd12cd01ac9cf63771
Oleg <antoshinov@gmail.com>
2024-07-17 18:01:27 +1000

    adding all

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from hexlet_django_blog.article import views
+from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView, ArticleFormEditView
 
 urlpatterns = [
-    path('', views.MyView.as_view()),
-    path('<str:tags>/<int:article_id>/', views.index, name='article'),
+    path('', IndexView.as_view(), name='articles'),
+    path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
+    path('<int:id>/', ArticleView.as_view(), name='current_article'),
+    path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,14 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse
+from .models import Article
+from .forms import ArticleForm
+from django.contrib import messages
 
-class MyView(View):
+class ArticleFormCreateView(View):
 
     def get(self, request, *args, **kwargs):
-        return redirect(reverse('article', kwargs={'tags': 'python', 'article_id': 42}))
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles')
+        messages.error(request, 'Data is not valid.')
+        return render(request, 'articles/create.html', context={'form': form})
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={'articles': articles,})
 
 
-def index(request, tags, article_id):
-    return HttpResponse(f'Статья номер {article_id}. Тег {tags}')
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html', context={'article': article,})
+
+
+class ArticleFormEditView(View):
+
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(request, 'articles/update.html', context={'form': form, 'article_id':article_id})
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            messages.success(request, 'Article details updated.')
+            form.save()
+            return redirect('articles')
+        messages.error(request, 'Incorrect data.')
+        return render(request, 'articles/update.html', {'form': form, 'article_id':article_id})
